2b4705aa554c2acc347d41de0ee4ac41c46cdf4a
trevordanahy <danat19@protonmail.com>
2021-01-06 10:55:57 -0600

    Changed reliefweb api method to post, it allows more params to be added easily and allows one api call instead of two.

--- main.py
+++ main.py
@@ -19,12 +19,8 @@ async def startup_event():
   session = aiohttp.ClientSession()
 
 
-async def get_reliefweb(status):
-  async with session.get(reliefweb.url, params = {
-    "appname": "LastWeekEarth",
-    "filter[field]": "status",
-    "filter[value]": status,
-    "fields[include][]": reliefweb.param_fields}) as response:
+async def get_reliefweb():
+  async with session.post(reliefweb.url, json=reliefweb.params) as response:
     res = await response.json()
     cleaned_events = [event for event in res["data"]]
     event_list = {"events": cleaned_events}
@@ -56,11 +52,9 @@ async def Reliefweb_Provider():
   fetches current humanitarian with current and alert status from the 
   UN OCHA Reliefweb API and returns the json results
   """
-  data = await asyncio.gather(
-    get_reliefweb("current"),
-    get_reliefweb("alert"))
+  data = await get_reliefweb()
   events = await reliefweb.general_parser(data)
-  return events
+  return data
 
 
 #Async HTTP request setup end
--- reliefweb.py
+++ reliefweb.py
@@ -3,9 +3,21 @@ from bs4 import BeautifulSoup
 
 
 url = "https://api.reliefweb.int/v1/disasters"
-param_fields = [ 
+return_fields = [ 
   "description-html", "status", "primary_country", "primary_type", "url_alias", "date"
   ]
+params = {
+  "appname": "lastweekearth",
+  "filter": {
+    "field": "status",
+    "value": ["current", "alert"],
+    "operator": "OR"
+  },
+  "fields": {
+    "include": return_fields
+  },
+  "limit": 50
+}
 
 async def update_parser(description):
   updates = []
@@ -36,25 +48,23 @@ async def img_parser(event_type):
 
 async def general_parser(data):
   events = []
-
-  for source in data:
-    for event in source["events"]:
-      description = event["fields"]["description-html"]
-      event_tmp = {}
-      event_tmp["source"] = "reliefweb"
-      event_tmp["id"] = event["id"]
-      event_tmp["status"] = event["fields"]["status"]
-      event_tmp["imgPath"] = "placeholder"
-      event_tmp["title"] = event["fields"]["name"]
-      event_tmp["category"] = event["fields"]["primary_type"]["name"]
-      event_tmp["updates"] = await update_parser(description)
-      #description grabs the 1st update as it is the most descriptive of the event. 
-      event_tmp["description"] = event_tmp["updates"][0][1]
-      event_tmp["path"] = None
-      #the location from reliefweb is listed in lon lat order in a dict , we want the location in a list with lat first
-      event_tmp["location"] = [point for point in event["fields"]["primary_country"]["location"].values()][::-1]
-      event_tmp["date"] = event["fields"]["date"]["created"]
-      events.append(event_tmp)
+  for event in data["events"]:
+    description = event["fields"]["description-html"]
+    event_tmp = {}
+    event_tmp["source"] = "reliefweb"
+    event_tmp["id"] = event["id"]
+    event_tmp["status"] = event["fields"]["status"]
+    event_tmp["imgPath"] = "placeholder"
+    event_tmp["title"] = event["fields"]["name"]
+    event_tmp["category"] = event["fields"]["primary_type"]["name"]
+    event_tmp["updates"] = await update_parser(description)
+    #description grabs the 1st update as it is the most descriptive of the event. 
+    event_tmp["description"] = event_tmp["updates"][0][1]
+    event_tmp["path"] = None
+    #the location from reliefweb is listed in lon lat order in a dict , we want the location in a list with lat first
+    event_tmp["location"] = [point for point in event["fields"]["primary_country"]["location"].values()][::-1]
+    event_tmp["date"] = event["fields"]["date"]["created"]
+    events.append(event_tmp)
 
   return events
 
